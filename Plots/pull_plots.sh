ae4f836efd650df0407da8c977e8fe67645f8094
Peter Yoachim <yoachim@uw.edu>
2015-04-11 21:59:03 -0700

    initial commit

---
+++ Plots/pull_plots.sh
@@ -0,0 +1,10 @@
+rsync -ar ~/Research/GLSB/Analysis/Plots/* /astro/users/yoachim/Dropbox/Writing/GLSB/Plots/
+
+rsync -ar ~/Research/GLSB/THINGS/*.eps /astro/users/yoachim/Dropbox/Writing/GLSB/Plots/
+
+rsync -ar ~/Research/GLSB/SDSS/dr10/*.eps /astro/users/yoachim/Dropbox/Writing/GLSB/Plots/
+
+#rsync -ar ~/Research/GLSB/SDSS/Color_Magnitu_Diagram.eps /astro/users/yoachim/Dropbox/Writing/GLSB/Plots/
+
+#rsync -ar ~/Research/GLSB/SDSS/mg_sigma.eps /astro/users/yoachim/Dropbox/Writing/GLSB/Plots/
+
